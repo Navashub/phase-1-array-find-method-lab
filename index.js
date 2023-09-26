380728f8c052e0e9bd28d159bc92b33c26ea94f0
Navashub <navasmuller01@gmail.com>
2023-09-26 07:29:38 +0300

    task completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record, year, result){
+    const winYear = record.find(function(games){
+        return games.result === "W";
+    });
+    return winYear ? winYear.year : undefined;
+}
